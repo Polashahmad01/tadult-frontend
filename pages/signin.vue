6cc957c3cdd9172a67657f5c13d9cc76f6f6a5ac
Polashahmad01 <dev.polashahmad@gmail.com>
2022-08-29 23:05:33 +0600

    Improve signin page & setup the logic

---
+++ pages/signin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-xl">
+    <app-sign-in />
+  </div>  
+</template>
+  
+<script>
+import AppSignIn from "~/components/app-signin.vue"
+  
+export default {
+  name: 'SigninPage',
+  components: {
+    AppSignIn,
+  }
+}
+</script>
+  
+<style lang="scss" scoped>
+.container-xl {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+  
