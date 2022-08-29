ba59b5fa4fc64dbb872dc1a91b302d5e605abe47
Polashahmad01 <dev.polashahmad@gmail.com>
2022-08-29 23:44:38 +0600

    User can signup as content creator

---
+++ pages/signup/content-creator/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-xl">
+    <app-signup-as-content-creator />
+  </div>  
+</template>
+  
+<script>
+import AppSignupAsContentCreator from "~/components/app-signup-as-content-creator.vue"
+  
+export default {
+  name: 'SignupAsContentCreatorPage',
+  components: {
+    AppSignupAsContentCreator,
+  }
+}
+</script>
+  
+<style lang="scss" scoped>
+.container-xl {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+  
